bf29fedd8824018bb21d37e8daf741047a6c8c0b
Your Name <dockerapp@zlaya.ru>
2017-11-16 14:12:08 +0500

    v2

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ RUN apk add --update openssh
 RUN sed -iE 's/^\#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config \
 	&& mkdir /data \
 	&& echo -e "Match User root\n\tX11Forwarding no\n\tAllowTcpForwarding no\n\tForceCommand internal-sftp\n\tChrootDirectory /data" >> /etc/ssh/sshd_config
+RUN	cp /etc/ssh /etc/bak.ssh
 
 ADD start.sh /usr/sbin/start.sh
 CMD ["/usr/sbin/start.sh"]
--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 for t in dsa ecdsa ed25519 rsa rsa1; do
-  if ! [ -e "/etc/ssh/ssh_host_${t}_key" ]; then
+  if ! [ -f "/etc/ssh/ssh_host_${t}_key" ]; then
     ssh-keygen -f "/etc/ssh/ssh_host_${t}_key" -t ${t} -N ""
   fi
 done
+if ! [ -f "/etc/ssh/sshd_config" ]; then cp /etc/bak.ssh/sshd_config /etc/ssh/; fi
+if ! [ -d "/etc/ssh/moduli" ]; then cp /etc/bak.ssh/moduli /etc/ssh/; fi
+
 PASS=${PASS:-p@zzw0rd}
 (echo ${PASS};echo ${PASS}) | passwd
 /usr/sbin/sshd -D
