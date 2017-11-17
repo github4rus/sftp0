a62ca4a49b86ed29377a346df5cf9f6711b93c3d
Your Name <dockerapp@zlaya.ru>
2017-11-17 08:40:26 +0500

    v2

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-for t in dsa ecdsa ed25519 rsa rsa1; do
+for t in dsa ecdsa ed25519 rsa; do
   if ! [ -f "/etc/ssh/ssh_host_${t}_key" ]; then
     ssh-keygen -f "/etc/ssh/ssh_host_${t}_key" -t ${t} -N ""
   fi
 done
 if ! [ -f "/etc/ssh/sshd_config" ]; then cp /etc/bak.ssh/sshd_config /etc/ssh/; fi
-if ! [ -d "/etc/ssh/moduli" ]; then cp -r /etc/bak.ssh/moduli /etc/ssh/; fi
+if ! [ -d "/etc/ssh/moduli" ]; then cp /etc/bak.ssh/moduli /etc/ssh/; fi
 
 PASS=${PASS:-p@zzw0rd}
 (echo ${PASS};echo ${PASS}) | passwd
